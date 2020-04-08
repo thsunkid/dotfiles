f7b987a6f6a07945606e7c9e104cfc5f429db43b
Thu Ng <thesunkid19@gmail.com>
2020-04-08 19:17:07 +0000

    change, project file

--- project/cond_cap.sh
+++ project/cond_cap.sh
@@ -27,6 +27,7 @@ sudo apt-get install default-jre &&
 pip install --upgrade cython &&
 pip install git+https://github.com/salaniz/pycocoevalcap &&
 pip install matplotlib &&
+git clone https://github.com/ruotianluo/cider.git
 cp -r /vinai/thunm15/thunm15/project/self-critical.pytorch/coco-caption/pycocoevalcap/spice/lib/stanford-corenlp-3.6.0* /home/ubuntu/.local/lib/python3.7/site-packages/pycocoevalcap/spice/lib/ &&
 pip install wandb==0.8.30 &&
 
@@ -35,5 +36,6 @@ cp /vinai/thunm15/per-project/modfiles/eval.py /home/ubuntu/.local/lib/python3.7
 cp /vinai/thunm15/per-project/modfiles/file_pusher.py /home/ubuntu/.local/lib/python3.7/site-packages/wandb/ && #a:78
 cp /vinai/thunm15/per-project/modfiles/run_manager.py /home/ubuntu/.local/lib/python3.7/site-packages/wandb/ && #d:1438,1439
 cp /vinai/thunm15/per-project/modfiles/tokenization_utils.py /home/ubuntu/.local/lib/python3.7/site-packages/pytorch_transformers/ #r:819, a:725
+cp /vinai/thunm15/per-project/modfiles/ciderD_scorer.py cider/pyciderevalcap/ciderD/ # add:93,05 n para into self.cook_append to control the number of ngram; change:80 'data' folder -> 'pcap_jsons' folder
 
 
