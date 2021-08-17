39cd5ffb5d4afd2f3b7dcc0d0f4271b6566263cb
thsunkid <thsunkid@gmail.com>
2021-08-17 13:31:00 +0700

    change zsh config - rm old dir

--- vimzsh.sh
+++ vimzsh.sh
@@ -10,6 +10,9 @@ cp ~/.dotfiles/zsh/.zshrc ~/ &&
 cp -r ~/.dotfiles/vim/.vim ~/ &&
 cp ~/.dotfiles/vim/.vimrc ~/ &&
 
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 #set zsh as default shell
 sudo chsh -s $(which zsh) &&
 echo "zsh" >> .bashrc &&
